fbe7899570d73e288aeb2a922fdfd6eb71293bcc
Drake <150128459+VialsShiny@users.noreply.github.com>
2025-11-13 18:29:01 +0100

    Adding Data

---
+++ src/app/data/info.jsx
@@ -0,0 +1,22 @@
+export const portfolioData = {
+  prenom: "Thibault",
+  nom: "Vialatou",
+  age: 17,
+  titre: "Développeur Full Stack",
+  localisation: "Paris, France",
+  email: "vialscorp@hotmail.com",
+  github: "github.com/VialsShiny",
+  linkedin: "linkedin.com/in/thibault-vialatou-44baa8307",
+  bio: "Passionnée par le développement web et la création d'expériences utilisateur exceptionnelles. Spécialisée en React et Laravel.",
+  competences: [
+    { nom: "React", niveau: 80 },
+    { nom: "Laravel", niveau: 95 },
+    { nom: "PHP", niveau: 90 },
+    { nom: "CSS/Tailwind", niveau: 95 }
+  ],
+  projets: [
+    { titre: "Piscine Atlantis", description: "Site vitrine mettant en valeur l’expertise de l’entreprise dans les piscines, spas et équipements bien-être, avec un design moderne et une navigation intuitive." },
+    { titre: "Resto App", description: "Application web de vente de plats (pizza, burger, etc.) permettant de gérer commandes et menus de manière efficace, avec une interface utilisateur conviviale." },
+    { titre: "Mini Social", description: "Mini réseau social interactif offrant un flux de contenus en temps réel pour découvrir de nouveaux posts et interagir facilement avec la communauté." }
+  ]
+};
