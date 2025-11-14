953fa703df2e08591c7c1530c32d464138210b4a
VialsShiny <vialsvialatou@gmail.com>
2025-11-14 10:42:26 +0100

    SEO optimization

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,34 +1,35 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Mon Portfolio | Développeur Web",
-  description: "Découvrez mes projets et compétences en développement web, React, Next.js et animations GSAP.",
-  keywords: "Portfolio, Développeur Web, React, Next.js, GSAP, Animations, Frontend",
-  authors: [{ name: "VialsShiny" }],
-  // openGraph: {
-  //   title: "Mon Portfolio | Développeur Web",
-  //   description: "Découvrez mes projets et compétences en développement web, React, Next.js et animations GSAP.",
-  //   url: "https://votre-portfolio.com",
-  //   siteName: "Portfolio",
-  //   images: [
-  //     {
-  //       url: "https://votre-portfolio.com/og-image.png",
-  //       width: 1200,
-  //       height: 630,
-  //       alt: "Aperçu de mon portfolio",
-  //     },
-  //   ],
-  //   locale: "fr_FR",
-  //   type: "website",
-  // },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title: "Mon Portfolio | Développeur Web",
-  //   description: "Découvrez mes projets et compétences en développement web, React, Next.js et animations GSAP.",
-  //   images: ["https://votre-portfolio.com/og-image.png"],
-  //   creator: "@VotreTwitter",
-  // },
+  title: "Thibault Vialatou | Développeur Fullstack",
+  description: "Portfolio de Thibault Vialatou, développeur web Fullstack spécialisé en React, Javascript et PHP.",
+  keywords: "Portfolio, Développeur Web, React, Next.js, Animations, Frontend, Backend, Php, Thibault Vialatou",
+  authors: [{ name: "Thibault Vialatou" }],
+  openGraph: {
+    title: "Thibault Vialatou | Développeur Fullstack",
+    description: "Portfolio de Thibault Vialatou, développeur web Fullstack spécialisé en React, Javascript et PHP.",
+    url: "https://vials-shiny.vercel.app",
+    siteName: "Portfolio",
+    images: [
+      {
+        url: "https://vials-shiny.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Aperçu du portfolio de Thibault Vialatou",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Thibault Vialatou | Développeur Fullstack",
+    description: "Portfolio de Thibault Vialatou, développeur web Fullstack spécialisé en React, Javascript et PHP.",
+    images: ["https://vials-shiny.vercel.app/og-image.png"],
+    creator: "@VialsShiny",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
