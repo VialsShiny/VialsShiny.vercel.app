6d9952835463e600b75100a3212c7c378373f253
VialsShiny <vialsvialatou@gmail.com>
2025-11-14 11:31:06 +0100

    Adding Google Analytics (Test)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -34,7 +35,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body cz-shortcut-listen="true">{children}</body>
+      <body>
+        {children}
+
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-VDNE4VSP8W"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VDNE4VSP8W');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
